df70af5af4da188eed44f257a74131f5a86815d8
Graydon Hoare <graydon@pobox.com>
2022-07-22 15:15:01 -0700

    add doc on assets-and-authorization, with auth part filled in

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "Stellar Jump Cannon Documentation",
+  title: "Soroban Documentation",
   // tagline: "Dinosaurs are cool",
   baseUrl: "/",
   onBrokenLinks: "warn",

6f7928bf431f38c2c8478c98b16b9e7091596d88
Tyler van der Hoeven <hi@tyvdh.com>
2022-08-11 11:19:13 -0400

    add a little preview flag

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,7 +6,7 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: "Soroban Documentation",
+  title: "Soroban Documentation [PREVIEW]",
   // tagline: 'Dinosaurs are cool',
   url: "https://stellar.org", // Your website URL
   baseUrl: "/",

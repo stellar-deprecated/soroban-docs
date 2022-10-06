f31d8affce022009599716ad5183d347e8a738f9
Tyler van der Hoeven <hi@tyvdh.com>
2022-10-06 17:52:19 -0400

    slight rename

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -93,7 +93,7 @@ const config = {
             ],
           },
           {
-            title: 'More',
+            title: 'Resources',
             items: [
               {
                 label: 'GitHub',

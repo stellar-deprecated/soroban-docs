dc560c91263bde6b8e361441192f1e2a2f3fb3a2
Tyler van der Hoeven <hi@tyvdh.com>
2022-10-06 16:13:29 -0400

    feedback tweaks

--- src/pages/index.js
+++ src/pages/index.js
@@ -59,7 +59,6 @@ export default function Home() {
               </div>
 
               <ul className="hp--back">
-                <li>A local sandbox for fast setup and iterative development</li>
                 <li>Stellar speed and reliability – with 5 seconds to finality per contract –</li>
                 <li>On a proven  network that has been live since 2015 and is processing 150 tps as we speak</li>
                 <li>A global anchor network of on/off ramps and assets, including worldwide cash access</li>
@@ -68,16 +67,18 @@ export default function Home() {
           </div>
 
           <p className="hp--disclaim">
-            It's still in development, and it won't be ready for production until 2023, but you can start experimenting with it right now! Not only that, but you can earn rewards for doing so!
+            The Soroban test network has launched! Start experimenting and earning rewards now!
           </p>
         </div>
 
         <div className='hp--cta hp--maxWidth'>
           <h2>Tinker &amp; Earn Rewards</h2>
-          <img src='./img/homepage/sorobanathon.svg' />
+          <a href="https://github.com/stellar/sorobanathon" className='hp--imgLink'>
+            <img src='./img/homepage/sorobanathon.svg' />
+          </a>
           <h1>Sorobanathon: First Light</h1>
           <p>
-            Between now and December 15, 2022, join Sorobanathon: First Light, experiment with Soroban, and submit a code example, issue, tutorial, blog post, issue, or video documenting your experience. Qualified submissions will receive $25-300 worth of USDC. It's a fun, low-lift way to tinker, test things out, and provide valuable feedback to both the Soroban development team and the Soroban ecosystem as a whole. Let's bootstrap this thing together!
+            Between now and December 15, 2022, join Sorobanathon: First Light. Experiment with Soroban and submit a code example, issue, tutorial, blog post, issue, or video documenting your experience. Qualified submissions will receive $25-300 worth of USDC. It's a fun, low-lift way to learn, test things out, and provide valuable feedback to both the Soroban development team and the Soroban ecosystem as a whole. Let's bootstrap this thing together!
           </p>
           <a href="https://github.com/stellar/sorobanathon" className='hp--button'>Join the Sorobanathon</a>
         </div>
@@ -87,15 +88,23 @@ export default function Home() {
             Connect with developers, <br />
             be the first to get updates
           </h2>
-          <img src='./img/homepage/discord.svg' />
-          <h1>Join the Discord </h1>
+          <a href="https://discord.gg/6ZCcYqbC96" className='hp--imgLink'>
+            <img src='./img/homepage/discord.svg' />
+          </a>
+          <h1>Join the Discord</h1>
           <p>
-            Sorobanathon: First Light is the first of many programs that draws from the $100M Soroban Adoption Fund, which the Stellar Development Foundation created to support the growth and development of the Soroban ecosystem. We'll be sharing more about additional Adoption Fund programs on the Stellar Dev Discord in the not-too-distant, so make sure to join today to be the first to hear those announcements.
+            Sorobanathon: First Light is the first of many programs that draws from the $100M Soroban Adoption Fund, which the Stellar Development Foundation created to support the growth and development of the Soroban ecosystem. We'll be sharing more about additional Adoption Fund programs on the Stellar Dev Discord in the not-too-distant future, so make sure to join today to be the first to hear those announcements.
           </p>
           <a href="https://discord.gg/6ZCcYqbC96" className='hp--button'>Join the Discord</a>
         </div>
 
-        <a href="/docs" className='hp--docsLink hp--button'>Read The Docs</a>
+        <div className='hp--cta hp--maxWidth'>
+          <h1>Start your Journey</h1>
+          <p>
+            Get set up, see tutorials and examples, <br/> and read more about the nuts and bolts of Soroban.
+          </p>
+          <a href="/docs" className='hp--button'>Read the Docs</a>
+        </div>
       </main>
     </Layout>
   )

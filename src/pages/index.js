b9bd9afca3bdc48509530e7b8dd8af35a417d7ff
Anuxhya <36203801+achallagundla@users.noreply.github.com>
2023-02-07 12:35:08 -0500

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -92,7 +92,7 @@ export default function Home() {
           </a>
           <h1>Soroban Adoption Fund</h1>
           <p>
-            SDF recently launched a $100 million Soroban Adoption Fund with the goal of accelerating Soroban’s adoption. Our first rewards program, <a href='https://github.com/stellar/sorobanathon'><i>Sorobanathon: First Light</i></a>, ended December 15, but fear not!  We'll be launching new Soroban-related programs soon.  In the meantime, if you or your organization is interested in building new tools or applications on Soroban and would like to receive additional information on Soroban Adoption Fund programs as they are rolled out, please complete the Soroban Adoption Fund interest form.
+            The $100 million Soroban Adoption Fund is a series of SDF-organized programs aiming to accelerate Soroban’s adoption. <a href='https://soroban.stellar.org/sorobanathon'><i>Sorobanathon: Equinox/i></a>, ended December 15, but fear not!  We'll be launching new Soroban-related programs soon.  In the meantime, if you or your organization is interested in building new tools or applications on Soroban and would like to receive additional information on Soroban Adoption Fund programs as they are rolled out, please complete the Soroban Adoption Fund interest form.
           </p>
           <a href="https://share.hsforms.com/1U3FFLQBPQzCLKtv1vS8T-Q4u978" className='hp--button'>Fill out the Form</a>
         </div>

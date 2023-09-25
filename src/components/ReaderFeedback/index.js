d94f3b54aec44d9ba5385b813006f6487aaba70d
Elliot Voris <elliot@stellar.org>
2023-09-25 09:36:06 -0500

    Including an `alt` tag for the like/dislike buttons

--- src/components/ReaderFeedback/index.js
+++ src/components/ReaderFeedback/index.js
@@ -31,8 +31,8 @@ const ReaderFeedback = ({ pageId }) => {
       ) : (
         <>
           Did you find this page helpful?
-          <IconThumbsUp className="feedback_thumbsup" onClick={() => giveFeedback(1)} />
-          <IconThumbsDown className="feedback_thumbsdown" onClick={() => giveFeedback(0)} />
+          <IconThumbsUp className="feedback_thumbsup" alt="Like" onClick={() => giveFeedback(1)} />
+          <IconThumbsDown className="feedback_thumbsdown" alt="Dislike" onClick={() => giveFeedback(0)} />
         </>
       )}
     </div>

55a557d8105d9bb69586524c80f1c5341cbbc53b
Elliot Voris <elliot@stellar.org>
2023-09-26 15:42:22 -0500

    adding my own test gtag?

--- src/analytics-module.js
+++ src/analytics-module.js
@@ -24,13 +24,21 @@ module.exports = () => ({
         {
           tagName: 'script',
           innerHTML: `
-            <!-- Google Tag Manager -->
-            <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-N6H4MQRW');</script>
-            <!-- End Google Tag Manager -->
+            })(window,document,'script','dataLayer','GTM-N6H4MQRW');
+          `
+        },
+        {
+          tagName: 'script',
+          innerHTML: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-NW8XTC5T');
           `
         }
       ],
@@ -55,6 +63,13 @@ module.exports = () => ({
           tagName: 'noscript',
           innerHTML: `
             <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N6H4MQRW"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>
+          `
+        },
+        {
+          tagName: 'noscript',
+          innerHTML: `
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NW8XTC5T"
             height="0" width="0" style="display:none;visibility:hidden"></iframe>
           `
         }

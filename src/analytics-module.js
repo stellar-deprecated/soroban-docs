f1db8adeac844b5cffeb813d6ca1caeb13bdb5c3
Elliot Voris <elliot@stellar.org>
2023-10-26 10:23:23 -0500

    remove staging tag manager containers in preparation for production

--- src/analytics-module.js
+++ src/analytics-module.js
@@ -21,26 +21,6 @@ module.exports = () => ({
             content: 'll8iy5eqaqpremxnd6bo2qg2qjxrtj',
           },
         },
-        {
-          tagName: 'script',
-          innerHTML: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-N6H4MQRW');
-          `
-        },
-        {
-          tagName: 'script',
-          innerHTML: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-NW8XTC5T');
-          `
-        }
       ],
       preBodyTags: [
         {
@@ -59,20 +39,6 @@ module.exports = () => ({
             height="0" width="0" style="display:none;visibility:hidden"></iframe>
           `
         },
-        {
-          tagName: 'noscript',
-          innerHTML: `
-            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N6H4MQRW"
-            height="0" width="0" style="display:none;visibility:hidden"></iframe>
-          `
-        },
-        {
-          tagName: 'noscript',
-          innerHTML: `
-            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NW8XTC5T"
-            height="0" width="0" style="display:none;visibility:hidden"></iframe>
-          `
-        }
       ],
     };
   },

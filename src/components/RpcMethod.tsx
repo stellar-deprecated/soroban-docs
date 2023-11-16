02f770b36930dd50c59401f3cbe6e9a2fdf2c667
Elliot Voris <elliot@stellar.org>
2023-11-16 15:30:49 -0600

    docs: put everything into one `openrpc.json` document
    - modify the metamask package to display things a little nicer
    - generate openrpc.json file from a compound schema (in another
      repo soon)
    - add more react components to the `<Method />` component

--- src/components/RpcMethod.tsx
+++ src/components/RpcMethod.tsx
@@ -2,10 +2,14 @@ import Method from "@metamask/open-rpc-docs-react";
 import React from "react";
 
 const CodeBlock = require('@theme/CodeBlock').default;
+const Tabs = require('@theme/Tabs').default;
+const TabItem = require('@theme/TabItem').default;
 
 export const RpcMethod = (param: { method:string; }) => {
-    const rpcMethod = require(`@site/static/rpc-methods/${param.method}.json`);
-    const wholeThing = require('@site/openrpc/restart/derefed-openrpc.json');
-    console.log(wholeThing)
-    return (<Method method={wholeThing.methods[0]} components={{CodeBlock}}/>);
+    const rpcDoc = require('@site/static/openrpc.json')
+    const rpcMethod = rpcDoc.methods.filter((meth: any) => meth.name === param.method )[0]
+
+    return (
+        <Method method={rpcMethod} components={{CodeBlock, Tabs, TabItem}} />
+    );
 };

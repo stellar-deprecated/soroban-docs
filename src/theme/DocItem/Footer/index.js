36552858a5f3b6005929c3bb9654b844fb0d32bf
Elliot Voris <elliot@stellar.org>
2023-11-28 11:45:35 -0600

    style: more selectively increase that top margin

--- src/theme/DocItem/Footer/index.js
+++ src/theme/DocItem/Footer/index.js
@@ -59,7 +59,7 @@ export default function DocItemFooter() {
   return (
     <>
       {canDisplayDocCardsOnGuide &&
-        <div className='margin-top--xl'>
+        <div className={clsx(metadata.permalink === '/guides/' ? 'margin-top--lg' : 'margin-top--xl')}>
           {metadata.permalink !== '/guides/' && <h3>Guides in this category:</h3>}
           <DocCardList />
         </div>

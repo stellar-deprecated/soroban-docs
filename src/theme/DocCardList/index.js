44aef740f0d1ee396d1914f5455c8a4de149cc0c
Elliot Voris <elliot@stellar.org>
2023-12-12 13:21:54 -0500

    change some styles of the tutorial list

--- src/theme/DocCardList/index.js
+++ src/theme/DocCardList/index.js
@@ -4,8 +4,12 @@ import {
   useCurrentSidebarCategory,
   filterDocCardListItems,
 } from '@docusaurus/theme-common';
+import {
+  useDocById,
+} from '@docusaurus/theme-common/internal';
 import DocCard from '@theme/DocCard';
 import styles from './style.module.css'
+import { title } from 'process';
 
 function DocCardListForCurrentSidebarCategory({className}) {
   const category = useCurrentSidebarCategory();
@@ -49,30 +53,39 @@ function GuidesDocList(props) {
 }
 
 function FilterableDocCardList(props) {
-  const [tutorialLevel, setTutorialLevel] = useState('All')
+  const [tutorialLevel, setTutorialLevel] = useState('All');
+  const [tutorialQuery, setTutorialQuery] = useState('');
 
   const {items, className} = props;
   const filteredItems = filterDocCardListItems(items);
+
   const filterDocCardsByTutorialLevel = (level) => {
     return filteredItems.filter(item => level.toLowerCase() === 'all' ? true : item.customProps?.tutorial?.level === level.toLowerCase())
   }
   let filteredDocCards = filterDocCardsByTutorialLevel(tutorialLevel)
 
+  const filterDocCardsByTutorialQuery = (query) => {
+    return filteredDocCards.filter(item => query !== '' ? item.label.toLowerCase().includes(query.toLowerCase()) || item.description.toLowerCase().includes(query.toLowerCase()) : true)
+  }
+  filteredDocCards = filterDocCardsByTutorialQuery(tutorialQuery)
+
   return (
     <>
-      <label>Select Tutorial Level</label>
       <select className={styles.docCardFilterSelect} onChange={e => setTutorialLevel(e.target.value)}>
-        <option default>All</option>
+        <option value="All" default>Select Tutorial Level</option>
         <option>Beginner</option>
         <option>Intermediate</option>
         <option>Advanced</option>
       </select>
+      <input placeholder="Search Tutorial Title" className={styles.docCardFilterSearch} onChange={e => setTutorialQuery(e.target.value)} />
       <section className={clsx('row', className)}>
-        {filteredDocCards.map((item, index) =>
-          <article key={index} className="col col--12 margin-bottom--lg">
-            <DocCard item={item} />
-          </article>
-        )}
+        {filteredDocCards.map((item, index) => {
+          const doc = useDocById(item.docId ?? undefined);
+          item.description = item.description ?? doc?.description
+          return (
+            <p className='col col--12'><a href={item.href}><strong>{item.label}</strong></a> - {item.description}</p>
+          )
+        })}
       </section>
     </>
   )

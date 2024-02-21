287dc87882aefaf965ac97be1a40f1cc7324511f
Elliot Voris <elliot@stellar.org>
2024-02-21 16:21:00 -0600

    add a list of "already migrated" stuff to the sidebar

--- sidebars.js
+++ sidebars.js
@@ -23,5 +23,33 @@ module.exports = {
       href: 'https://developers.stellar.org/docs/tools/developer-tools',
       label: 'Developer Tools',
     },
+    {
+      type: 'category',
+      label: 'Reference',
+      collapsed: false,
+      items: [
+        {
+          type: 'category',
+          label: 'SDKs',
+          items: [
+            {
+              type: 'link',
+              href: 'https://developers.stellar.org/docs/tools/sdks/library',
+              label: 'Write Contracts',
+            },
+            {
+              type: 'link',
+              href: 'https://developers.stellar.org/docs/tools/sdks/library',
+              label: 'Interact with Contracts',
+            },
+            {
+              type: 'link',
+              href: 'https://developers.stellar.org/docs/tools/sdks/build-your-own',
+              label: 'Build Your Own SDK',
+            },
+          ],
+        },
+      ],
+    },
   ],
 };

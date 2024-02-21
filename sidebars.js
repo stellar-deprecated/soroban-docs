1df0d4dd0146dbfb49f405ecc15130a07f4aa37b
Elliot Voris <elliot@stellar.org>
2024-02-21 17:42:52 -0600

    Revert "linking to migrated developer tools page"
    This reverts commit fc2340818a08c42fc1f13f6a49d97111610c9100.

--- sidebars.js
+++ sidebars.js
@@ -8,48 +8,6 @@ module.exports = {
       type: 'link',
       label: 'Meeting Notes',
       href: '/meetings',
-    },
-    {
-      type: 'html',
-      value: '<hr />',
-    },
-    {
-      type: 'html',
-      value: 'Migrated Content:',
-      className: 'sidebar-title',
-    },
-    {
-      type: 'link',
-      href: 'https://developers.stellar.org/docs/tools/developer-tools',
-      label: 'Developer Tools',
-    },
-    {
-      type: 'category',
-      label: 'Reference',
-      collapsed: false,
-      items: [
-        {
-          type: 'category',
-          label: 'SDKs',
-          items: [
-            {
-              type: 'link',
-              href: 'https://developers.stellar.org/docs/tools/sdks/library',
-              label: 'Write Contracts',
-            },
-            {
-              type: 'link',
-              href: 'https://developers.stellar.org/docs/tools/sdks/library',
-              label: 'Interact with Contracts',
-            },
-            {
-              type: 'link',
-              href: 'https://developers.stellar.org/docs/tools/sdks/build-your-own',
-              label: 'Build Your Own SDK',
-            },
-          ],
-        },
-      ],
-    },
-  ],
+    }
+  ]
 };

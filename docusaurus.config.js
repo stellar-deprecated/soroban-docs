7683d812cb183536cdd8437717c1f86a5138999b
Elliot Voris <elliot@stellar.org>
2024-03-13 10:03:51 -0500

    change meetings navbar item to external link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -125,7 +125,7 @@ const config = {
             position: 'left'
           },
           {
-            to: 'https://developers.stellar.org/meetings',
+            href: 'https://developers.stellar.org/meetings',
             label: 'Meetings',
             position: 'right',
           },

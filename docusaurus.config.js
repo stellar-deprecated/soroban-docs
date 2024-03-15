5bd39a773291f03e8d47e3a51f7cc8ebe343c24f
Elliot Voris <elliot@stellar.org>
2024-03-15 10:13:35 -0500

    disable the "blog" feature now that it's migrated

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -54,15 +54,7 @@ const config = {
       "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
-        blog: {
-          path: 'meeting-notes',
-          blogTitle: 'Meeting Notes',
-          blogDescription: 'Notes and recordings from the Soroban protocol & developers meetings',
-          blogSidebarTitle: 'All meetings',
-          blogSidebarCount: 'ALL',
-          postsPerPage: 'ALL',
-          routeBasePath: 'meetings',
-        },
+        blog: false,
         docs: {
           showLastUpdateTime: true,
           showLastUpdateAuthor: true,

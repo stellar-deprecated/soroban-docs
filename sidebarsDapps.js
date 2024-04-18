5e937ee05139a516dfb313f1e58dc6b4ac2fd5d5
Elliot Voris <elliot@stellar.org>
2024-04-18 11:35:30 -0500

    add migration URLs to Dapps section's sidebar

--- sidebarsDapps.js
+++ sidebarsDapps.js
@@ -1,29 +1,49 @@
 module.exports = {
   mySidebar: [
     {
-      type: 'doc',
-      id: 'README',
+      type: 'link',
       label: 'Welcome',
+      href: 'https://developers.stellar.org/docs/learn/migrate/introduction-to-solidity-and-rust',
     },
     {
       type: 'link',
       label: 'Dashboard',
-      href: '/dashboard',
+      href: 'https://developers.stellar.org/docs/learn/interactive/dapps/dashboard',
     },
     {
-      type: 'doc',
-      id: 'scaffold-soroban',
+      type: 'link',
       label: 'Scaffold Soroban',
+      href: 'https://developers.stellar.org/docs/learn/interactive/dapps/scaffold-soroban',
     },
     {
       type: 'category',
       label: 'Guides',
       items: [
-        'guides/docker',
-        'guides/initialization',
-        'guides/react',
-        'guides/smart-contracts',
-        'guides/wallets'
+        {
+          type: 'link',
+          label: 'Docker',
+          href: 'https://developers.stellar.org/docs/smart-contracts/guides/dapps/docker'
+        },
+        {
+          type: 'link',
+          label: 'Initialization',
+          href: 'https://developers.stellar.org/docs/smart-contracts/guides/dapps/initialization'
+        },
+        {
+          type: 'link',
+          label: 'React',
+          href: 'https://developers.stellar.org/docs/smart-contracts/guides/dapps/react'
+        },
+        {
+          type: 'link',
+          label: 'Smart Contracts',
+          href: 'https://developers.stellar.org/docs/learn/interactive/dapps/challenges/challenge-0-crowdfund'
+        },
+        {
+          type: 'link',
+          label: 'Wallets',
+          href: 'https://developers.stellar.org/docs/smart-contracts/guides/freighter/integrate-freighter-react'
+        },
       ],
       collapsible: true,  // The category can be collapsed.
       collapsed: true,   // Initially, the category will be collapsed.
